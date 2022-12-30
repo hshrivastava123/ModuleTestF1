719bf9285e29a8c93cfb0a5191a5e8578296bfae
Kshitij Shrivastava <117522612+hshrivastava123@users.noreply.github.com>
2022-12-30 19:19:29 +0530

    code is done

---
+++ script.js
@@ -0,0 +1,11 @@
+function showm(){
+    var i1 = document.getElementById('ia');
+    var i2 = document.getElementById('ib');
+    var i3 = document.getElementById('ic');
+
+    if (i1.value === '' | i2.value==='' | i3.value===''){
+        
+    }else {
+        document.getElementById('MessageBox').style.opacity = '1';
+    }
+}
